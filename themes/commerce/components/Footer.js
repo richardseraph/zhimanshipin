87533dbc41bca53be8be36564ef2e28e2e6c1304
wuage <39831967+richardseraph@users.noreply.github.com>
2024-12-14 20:07:34 +0800

    Update Footer.js

--- themes/commerce/components/Footer.js
+++ themes/commerce/components/Footer.js
@@ -254,17 +254,6 @@ const Footer = props => {
               All Rights Reserved.
             </div>
 
-            {/* 技术支持 */}
-            <div className='text-xs text-light-500 dark:text-gray-700'>
-              Powered by{' '}
-              <a
-                href='https://github.com/tangly1024/NotionNext'
-                className='dark:text-gray-300'>
-                NotionNext {siteConfig('VERSION')}
-              </a>
-              .
-            </div>
-
             {/* 站点统计 */}
             <div>
               <span className='hidden busuanzi_container_site_pv'>
