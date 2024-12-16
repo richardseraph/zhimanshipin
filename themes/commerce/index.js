2857dedcb9127fbed68e65dc36d3cf86a2674791
wuage <39831967+richardseraph@users.noreply.github.com>
2024-12-16 19:15:08 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -256,19 +256,20 @@ const LayoutSlug = props => {
               <div className='flex md:flex-row flex-col w-full justify-between py-4'>
                 <div
                   id='left-img'
-                  className='md:w-1/4 flex justify-center items-center border'>
+                  className='md:w-1/2 flex justify-center items-center border'>
                   <LazyImage
                     src={headerImage}
                     className='m-auto w-full h-auto aspect-square object-cover object-center'
                   />
                 </div>
 
-                <div id='info-right' className='md:w-3/4 p-4'>
+                <div id='info-right' className='md:w-1/2 p-4'>
                   <div>{post?.title}</div>
                   <div
                     dangerouslySetInnerHTML={{ __html: post?.summary }}></div>
                 </div>
               </div>
+            )}
 
 
 
